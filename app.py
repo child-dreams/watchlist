aac4fc3d7523db623203b07e9d8dbf15daab819f
hao <2287294279@qq.com>
2021-07-11 19:00:11 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
+import os
+import sys
+import click
+
 from flask import Flask
-from flask import escape, url_for
+from flask_sqlalchemy import SQLAlchemy   #导入扩展类
 from flask import Flask, render_template
+#数据库配置
+WIN = sys.platform.startswith('win')
+if WIN: #如果是win系统，使用三个斜线
+	prefix = 'sqlite:///'
+else:  #否则使用四个斜线
+	prefix = 'sqlite:////'
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATTONS'] =False   #关闭对模型修改的监控
+
+db = SQLAlchemy(app)   #初始化扩展，传入程序实例app
 @app.route('/user/<name>')
 def user_page(name):
 	return 'User: %s' % escape(name)
@@ -22,4 +38,51 @@ movies = [
 
 @app.route('/')
 def index():
-	return render_template('index.html', name=name, movies=movies)
+	user = User.query.first()    #读取用户记录
+	movies = Movie.query.all()   #读取所有电影记录
+	return render_template('index.html', user=user, movies=movies)
+
+#创建数据库模型
+class User(db.Model):   #表名将会是user(自动生成，小写处理)
+	id = db.Column(db.Integer, primary_key=True)  #主键
+	name = db.Column(db.String(20))   #名字
+
+class Movie(db.Model):    #表名将会是movie
+	id = db.Column(db.Integer, primary_key=True)   #主键
+	title = db.Column(db.String(60))  #电影标题
+	year = db.Column(db.String(4))   #电影年份
+#自定义命令创建数据库表
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')   #设置选项
+def initdb(drop):
+	"""Initialize the database."""
+	if drop:  #判断是否输入了选项
+		db.drop_all()
+	db.create_all()
+	click.echo('Initialized database.')  #输出提示信息
+@app.cli.command()
+def forge():
+	"""Generate fake data."""
+	db.create_all()
+	#全局的两个变量移动到这个函数内
+	name = 'Hao Hao'
+	movies = [
+		{'title': 'My Neighbor Totoro', 'year': '1988'},
+		{'title': 'Dead Poets Society', 'year': '1989'},
+		{'title': 'A Perfect World', 'year': '1993'},
+        	{'title': 'Leon', 'year': '1994'},
+        	{'title': 'Mahjong', 'year': '1996'},
+        	{'title': 'Swallowtail Butterfly', 'year': '1996'},
+        	{'title': 'King of Comedy', 'year': '1999'},
+        	{'title': 'Devils on the Doorstep', 'year': '1999'},
+        	{'title': 'WALL-E', 'year': '2008'},
+        	{'title': 'The Pork of Music', 'year': '2012'},
+	]
+	user = User(name=name)
+	db.session.add(user)
+	for m in movies:
+		movie = Movie(title=m['title'], year=m['year'])
+		db.session.add(movie)
+
+	db.session.commit()
+	click.echo('Done.')
